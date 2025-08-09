2c414b7fbfbbf496d056554c1690408c6dca5417
Neo3691102 <nestor8772@gmail.com>
2025-08-08 21:28:23 -0600

    Se agregan la funcion addToLibrary que usa la funcion de useState, para cambiar el estado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import SearchResults from './components/searchResults/searchResults';
 import Library from './components/library/Library';
 import picture from "./img/beethoven-ludwig-van.jpg";
+import { useState } from 'react';
 
 
 const App = () => {
@@ -61,56 +62,15 @@ const App = () => {
       }
     ]
 
-    const librarysongs = [
-      {
-        id: 1,
-        songName: "Winter",
-        artist: "Vivaldi",
-        duration: "9:27",
-        album: "classic-songs",
-        picture: picture
-      },
-      {
-        id: 2,
-        songName: "Rondo Alla Turca",
-        artist: "Mozart",
-        duration: "3:32",
-        album: "classic-songs",
-        picture: picture
-      },
-      {
-        id: 3,
-        songName: "Lacrimosa",
-        artist: "Mozart",
-        duration: "4:05",
-        album: "classic-songs",
-        picture: picture
-      },
-      {
-        id: 4,
-        songName: "Moonlight Sonata",
-        artist: "Beethoven",
-        duration: "6:00",
-        album: "classic-songs",
-        picture: picture
-      },
-      {
-        id: 5,
-        songName: "Virus",
-        artist: "Beethoven",
-        duration: "3:35",
-        album: "classic-songs",
-        picture: picture
-      },
-      {
-        id: 6,
-        songName: "Apassionata",
-        artist: "Beethoven",
-        duration: "7:35",
-        album: "classic-songs",
-        picture: picture
-      }
-    ];
+     const [librarysongs, setLibrary] = useState([]);
+
+  // Función para agregar una canción a la biblioteca
+  const addToLibrary = (song) => {
+    // Evita duplicados
+    if (!librarysongs.some(s => s.id === song.id)) {
+      setLibrary([...librarysongs, song]);
+    }
+  };
 
 
     return (
@@ -122,7 +82,7 @@ const App = () => {
       </div>
 
       <div className="songList">
-        <SearchResults songs={songList} />
+        <SearchResults songs={songList} onAddToLibrary={addToLibrary} />
       </div>
       
     </div>
