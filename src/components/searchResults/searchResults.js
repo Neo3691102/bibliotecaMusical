81b318ccab68bcbcae702b500763316ebde0c64d
Neo3691102 <nestor8772@gmail.com>
2025-08-09 20:39:06 -0600

    Se agrega el input de busqueda

--- src/components/searchResults/searchResults.js
+++ src/components/searchResults/searchResults.js
@@ -5,6 +5,7 @@ import style from "../../components/searchResults/style.css";
 const SearchResults = (props) => {
     return (
         <>
+            
             {props.songs.map(song => (
                 <Song
                     key={song.id}
