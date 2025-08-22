904f21ff1e868a64777eb912ec7ecd2c568b9c87
Neo3691102 <nestor8772@gmail.com>
2025-08-22 11:39:46 -0600

    Se actualiza el codigo para poder agregar albums a la biblioteca

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,12 +1,12 @@
 import style from "./style.css";
-import LibrarySong from "../librarySong/librarySong";
+import LibraryAlbum from "../libraryAlbum/libraryAlbum";
 
 const Library = (props) => {
     return(
         <div className="library">
-            <h2 className="library__title">Biblioteca</h2>
+            <h2 className="library__title">Albums</h2>
                 <div className="library__container">
-                    <LibrarySong songs={props.songs} />
+                    <LibraryAlbum albums={props.albums} />
                 </div>
         </div>
             
