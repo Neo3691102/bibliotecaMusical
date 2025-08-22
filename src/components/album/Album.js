41e59947ab015ae48c37c5a3a4a13d316c80bd43
Neo3691102 <nestor8772@gmail.com>
2025-08-22 11:08:21 -0600

    Se modifican los componentes y se crean nuevos componentes para mostrar canciones y detalles

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,19 +1,19 @@
-import song from "./song.css";
+import album from "./album.css";
 import { Link } from "react-router-dom";
 
-const Song = (props) => {
+const Album = (props) => {
     return (
-        <div className="songCard">
+        <div className="albumCard">
             <img className="picture" src={props.strAlbumThumb} alt="imagen"/>
-            <h2 className="songTitle">{props.strAlbum}</h2>
+            <h2 className="albumtitle">{props.strAlbum}</h2>
             <p className="artist">{props.strArtist}</p>
-            <p className="album">{props.album}</p>
+            <p className="year">{props.intYearReleased}</p>
             <button className="button" onClick={props.onAddToLibrary}>
                 Agregar a la biblioteca
             </button>
-            <Link className="details-link" to={`/song/${props.idAlbum}`}>Ver detalles</Link>
+            <Link className="details-link" to={`/album/${props.idAlbum}`}>Ver detalles</Link>
         </div>
     );
 };
 
-export default Song;
+export default Album;
