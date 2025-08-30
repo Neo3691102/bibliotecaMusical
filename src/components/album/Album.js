d227a0bceed7b35b2eb852fcd32ecd8e7c8f40d8
Neo3691102 <nestor8772@gmail.com>
2025-08-30 12:57:15 -0600

    Se Refactorizan los estilos usando styled components para el componente Album

--- src/components/album/Album.js
+++ src/components/album/Album.js
@@ -1,18 +1,20 @@
-import album from "./album.css";
-import { Link } from "react-router-dom";
+
+
+import { AlbumStylesDiv, AlbumTitle, AlbumArtist, AlbumYear, AlbumButton, AlbumPicture, DetailLink} from "./AlbumStyles";
+
 
 const Album = (props) => {
     return (
-        <div className="albumCard">
-            <img className="picture" src={props.strAlbumThumb} alt="imagen"/>
-            <h2 className="albumtitle">{props.strAlbum}</h2>
-            <p className="artist">{props.strArtist}</p>
-            <p className="year">{props.intYearReleased}</p>
-            <button className="button" onClick={props.onAddToLibrary}>
+        <AlbumStylesDiv>
+            <AlbumPicture src={props.strAlbumThumb} alt="imagen"/>
+            <AlbumTitle>{props.strAlbum}</AlbumTitle>
+            <AlbumArtist>{props.strArtist}</AlbumArtist>
+            <AlbumYear className="year">{props.intYearReleased}</AlbumYear>
+            <AlbumButton onClick={props.onAddToLibrary}>
                 Agregar a la biblioteca
-            </button>
-            <Link className="details-link" to={`/album/${props.idAlbum}`}>Ver detalles</Link>
-        </div>
+            </AlbumButton>
+            <DetailLink to={`/album/${props.idAlbum}`}>Ver detalles</DetailLink>
+        </AlbumStylesDiv>
     );
 };
 
