d96c7100f88ef7249d42c0d1d72c1da339f74ca2
Neo3691102 <nestor8772@gmail.com>
2025-09-05 22:18:04 -0600

    Se agregan margenes izquierdo y derecho a spinner

--- src/components/Spinner/Loading.js
+++ src/components/Spinner/Loading.js
@@ -2,7 +2,7 @@ import { ClipLoader } from "react-spinners";
 
 const Spinner = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "330px" }}>
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "330px", marginLeft: "auto", marginRight: "auto" }}>
       <ClipLoader color="#3b5998" size={50} />
     </div>
   );
