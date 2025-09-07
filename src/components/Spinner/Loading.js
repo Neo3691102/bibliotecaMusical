5e53e63280f213722bba258efaba58537e3a36c3
Neo3691102 <nestor8772@gmail.com>
2025-09-06 21:04:01 -0600

    Se cambia el tamaño del spinner

--- src/components/Spinner/Loading.js
+++ src/components/Spinner/Loading.js
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 const Spinner = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: "330px", marginLeft: "auto", marginRight: "auto" }}>
-      <ClipLoader color="#3b5998" size={50} />
+      <ClipLoader color="#3b5998" size={60} />
     </div>
   );
 }
